809482b49f62d1ca6a75076c8771b5323c01d094
Alexander Shutov <shutovby@gmail.com>
2017-10-18 23:04:38 +0300

    Moved controls from malevic

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import withForms from 'malevic/forms';
+
+withForms();
+
+export { default as Button } from './button';
+export { default as CheckBox } from './checkbox';
+export { default as Flex } from './flex';
+export { default as Label } from './label';
+export { default as TextBox } from './textbox';
